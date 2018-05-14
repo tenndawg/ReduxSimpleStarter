e403e31b0ce52efdaca687a1c871c5db1edf7473
tenndawg <nickolas.tenney@gmail.com>
2018-05-14 11:12:06 -0600

    Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './ReducerBooks';
+import ActiveBook from './ReducerActiveBook';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
